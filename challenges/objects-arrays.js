1e589113094cfa7b252188e00aaf1a8dcf2a1021
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-14 11:26:00 -0400

    Created Stegosaurus Object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -18,6 +18,14 @@ const tyrannosaurus = ({
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = ({
+    name: 'Stegosaurus',
+    diet: 'herbivorous',
+    weight: '2000kg',
+    length: '9m',
+    period: 'Late Jurassic',
+});
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
 // Using your dinosaur objects, log answers to these questions:
