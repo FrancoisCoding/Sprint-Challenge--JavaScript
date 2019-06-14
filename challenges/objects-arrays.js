da27ecedde629c3a3892b2e0cc797e873a4c82a6
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-14 11:24:50 -0400

    Created Tyrannosaurus Object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,6 +8,14 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = ({
+    name: 'Tyrannosaurus',
+    diet: 'Carnivorous',
+    weight: '7000kg',
+    length: '12m',
+    period: 'Late Cretaceous',
+});
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
