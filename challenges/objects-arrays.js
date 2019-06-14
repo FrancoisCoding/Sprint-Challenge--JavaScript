9c8183f323686eae6473f933379821b6cbdcfd59
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-06-14 11:27:32 -0400

    Created Velociraptor Object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -20,13 +20,21 @@ const tyrannosaurus = ({
 
 const stegosaurus = ({
     name: 'Stegosaurus',
-    diet: 'herbivorous',
+    diet: 'Herbivorous',
     weight: '2000kg',
     length: '9m',
     period: 'Late Jurassic',
 });
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor = ({
+    name: 'Velociraptor',
+    diet: 'Carnivorous',
+    weight: '15kg',
+    length: '1.8m',
+    period: 'Late Cretaceous',
+});
 
 // Using your dinosaur objects, log answers to these questions:
 
